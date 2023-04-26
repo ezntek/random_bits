d0893681a78c18ffe79eaa6e8cea62d7aa94405e
ezntek <easontek@xflymusic.com>
2023-04-26 11:15:19 +0800

    oyuawqfhnduoyawndf

---
+++ py/calculator_functions.py
@@ -0,0 +1,11 @@
+import math
+
+def distance(a, b):
+    "for calculating the distance between 2 points on a graph"
+
+    return abs(a)+abs(b)
+
+def hyp(a, b):
+    "hypotenuse"
+
+    return math.sqrt(a**2+b**2)
