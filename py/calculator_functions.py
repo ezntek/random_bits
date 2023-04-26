32c6d3c48245bd12f538b701fd53a584ac536abc
ezntek <eason@ezntek.com>
2023-04-26 19:20:56 +0800

    add more calculator repl functions

--- py/calculator_functions.py
+++ py/calculator_functions.py
@@ -1,11 +1,11 @@
 import math
 
-def distance(a, b):
-    "for calculating the distance between 2 points on a graph"
+# some functions for use in a repl
 
-    return abs(a)+abs(b)
-
-def hyp(a, b):
-    "hypotenuse"
-
-    return math.sqrt(a**2+b**2)
+area_square = lambda x: x**2
+area_rect = lambda x, y: x*y
+area_trapezium = lambda top, bottom, height: ((top+bottom)/2)*height
+area_triangle = lambda base, height: (base*height)/2
+distance_graph = lambda a, b: abs(a)+abs(b)
+hyp = lambda a, b: math.sqrt(a**2+b**2)
+reverse_hyp = lambda c, b: math.sqrt(c**2-b**2)
