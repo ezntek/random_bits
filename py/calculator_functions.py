adf18cfe2c705b1624ff54f56d97258ab9c69be3
ezntek <eason@ezntek.com>
2023-04-26 19:50:40 +0800

    Update calculator_functions.py

--- py/calculator_functions.py
+++ py/calculator_functions.py
@@ -11,3 +11,7 @@ area_triangle = lambda base, height: (base*height)/2
 distance_graph = lambda a, b: abs(a)+abs(b)
 hyp = lambda a, b: math.sqrt(a**2+b**2)
 reverse_hyp = lambda c, b: math.sqrt(c**2-b**2)
+area_circle = lambda r: r**2*3.14
+area_circle_with_diameter = lambda d: (d/2)**2*3.14 
+area_circle_pro = lambda pi, r: pi*r**2
+area_circle_pro_with_diameter = lambda pi, d: (d/2)**2*pi
